d79bfddb4abc985b3977306d5c5268661057ca3e
Ryan Andonian <ryanandonian@gmail.com>
2017-08-13 20:17:55 -0700

    Adds environment switch

--- dist/src/game.js
+++ dist/src/game.js
@@ -1,5 +1,9 @@
 const DEBUG = window.location.href.indexOf('debug=true') !== -1;
-
+if (/localhost/.test(window.location.href)) {
+  console.info('ＥＮＶＩＲＯＮＭＥＮＴ: ＰＵＧＧＬＥ ＵＮＫＮＯＷＮＳ ＢＡＴＴＬＥＰＵＧＳ');
+} else {
+  console.info('ＥＮＶＩＲＯＮＭＥＮＴ: ＰＵＧ');
+}
 const game = new Phaser.Game(64, 64, Phaser.AUTO, 'game-container', new GameState());
 
 console.info('ＨＵＭＡＮ ＭＵＳＩＣ');
